f9e042a3c1a456158a08a90fa7c9f59ba7274ae4
panorama713 <yaiba713@gmail.com>
2022-02-15 17:20:19 +0900

    2/6

---
+++ work/01_HTML5/02_chapter/js/js/OuterJavaScript.js
@@ -0,0 +1,5 @@
+// 변수선언 = var
+var x = true, y = false;
+document.write((x && y) + "<br>"); //false 논리 and 연산
+document.write((x || y) + "<br>"); //true 논리 or연산
+document.write(!x); // false 논리 not연산
